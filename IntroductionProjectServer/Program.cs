453a72546242a4feee6198d3ed3ef5b668adc798
Ratska <toonkool14@gmail.com>
2022-01-08 12:05:09 +0100

    Add project files.

---
+++ IntroductionProjectServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IntroductionProjectServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "IntroductionProjectServer";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ IntroductionProjectServer/Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace IntroductionProjectServer
+{
+    class Server
+    {
+        public static int MaxPlayers { get; private set; }
+        public static int Port { get; private set; }
+
+        private static TcpListener tcpListener;
+
+
+
+    }
+}
